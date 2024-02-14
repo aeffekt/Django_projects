4f74f0d941fabc4e361229485e832c716438bc65
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-02-14 12:41:20 -0300

    login-logout de to-do-list

--- to_do_list/todo/urls.py
+++ to_do_list/todo/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ItemCreateView, ItemDeleteView, ItemDetailView, ItemListView, ItemUpdateView
+from .views import (ItemCreateView, ItemDeleteView, ItemDetailView, 
+                    ItemListView, ItemUpdateView, CustomLoginView)
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", ItemListView.as_view(), name="list-item"),
-    path("todo/item/create/", ItemCreateView.as_view(), name="item-create"),
-    path("todo/item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
-    path("todo/item/<int:pk>/delete", ItemDeleteView.as_view(), name="item-delete"),        
-    path("todo/item/<int:pk>/update", ItemUpdateView.as_view(), name="item-update"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page='login'), name="logout"),
+    path("", ItemListView.as_view(), name="item-list"),
+    path("item-create/", ItemCreateView.as_view(), name="item-create"),
+    path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
+    path("item/<int:pk>/delete", ItemDeleteView.as_view(), name="item-delete"),        
+    path("item/<int:pk>/update", ItemUpdateView.as_view(), name="item-update"),
 ]
--- to_do_list/todo/views.py
+++ to_do_list/todo/views.py
@@ -1,24 +1,52 @@
-from django.shortcuts import render
 from .models import Item
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ItemDetailView(DetailView):
+class CustomLoginView(LoginView):
+    template_name = 'todo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('item-list')
+
+
+class ItemDetailView(LoginRequiredMixin, DetailView):
     model = Item
     context_object_name = "item"
 
-class ItemListView(ListView):
+
+class ItemListView(LoginRequiredMixin, ListView):
     model = Item
     context_object_name = "items"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['items'] = context['items'].filter(user=self.request.user)
+        context['items'] = context['items'].filter(done_status=False)
+        return context
 
-class ItemCreateView(CreateView):
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
+    model = Item    
+    fields = {'title', 'description', 'done_status'}
+    success_url = reverse_lazy('item-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ItemCreateView, self).form_valid(form)
+
+
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
     model = Item
+    fields = '__all__'
+    success_url = reverse_lazy('item-list')
 
 
-class ItemUpdateView(UpdateView):
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
     model = Item
-
-
-class ItemDeleteView(DeleteView):
-    model = Item
+    context_object_name = "item"
+    success_url = reverse_lazy('item-list')
