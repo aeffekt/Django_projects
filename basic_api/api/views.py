c70b8224511a4e8c42472c92ccb48ab9288bb9ff
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-02-15 15:49:44 -0300

    basic API finalizada crud to-do-list

--- basic_api/api/views.py
+++ basic_api/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    # 👇 1. Add this line for authentication
+    permission_classes = [permissions.IsAuthenticated]
+
+class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
